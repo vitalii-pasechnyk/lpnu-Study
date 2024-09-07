6f05b52cf6adaabdb51d8e5a3b74980cc31229c3
vitalii-pasechnyk <w.pasechnyk@gmail.com>
2024-09-07 13:50:24 +0300

    Task1 has done.

---
+++ src/Lab1/Task1.java
@@ -0,0 +1,14 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Програма для перетворення заданого цілого числа у двійкову систему.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть ціле число: ");
+        int n = sc.nextInt();
+        String toBin = Integer.toBinaryString(n);
+        System.out.println("Результат: " + toBin);
+    }
+}
