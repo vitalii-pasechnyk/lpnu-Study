5827e5a8ac177640833bd1ed69d0e7927a241290
vitalii-pasechnyk <w.pasechnyk@gmail.com>
2024-09-07 13:54:05 +0300

    Task3 has done.

---
+++ src/Lab1/Task3.java
@@ -0,0 +1,21 @@
+package Lab1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int n = 100;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
