94e81927029fbffe52b1badf25fc139c48100a3f
vitalii-pasechnyk <w.pasechnyk@gmail.com>
2024-09-08 22:37:25 +0300

    Task2 has done.

--- src/Lab1/Task2.java
+++ src/Lab1/Task2.java
@@ -1,7 +1,37 @@
 package Lab1;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Простий парсер виразів.");
+        System.out.println("Наприклад: 2 + 4 ...");
+        System.out.println("Введіть простий вираз:");
+        String problem = input.nextLine();
 
+        String[] split = problem.split(" ");
+        double a = Double.parseDouble(split[0]);
+        String d = split[1];
+        double b = Double.parseDouble(split[2]);
+
+        double result = 0;
+        switch (d) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                System.out.println("Некорректна дія.");
+        }
+        System.out.println(a + " " + d + " " + b +  " = " + result);
     }
 }
